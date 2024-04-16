f8957583e82807a8c7cc123637a07cda8d821b78
vagrant <vagrant@homestead>
2024-04-16 01:20:12 +0000

    Contain Book Responses in a data Attribute

---
+++ app/Http/Controllers/BooksController.php
@@ -0,0 +1,84 @@
+<?php
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use App\Book;
+/**
+ * Class BooksController
+ * @package App\Http\Controllers
+ */
+class BooksController
+{
+    /**
+     * GET /books
+     * @return array
+     */
+    public function index()
+    {
+        return ['data' => Book::all()->toArray()];
+    }
+
+    /**
+     * GET /books/{id}
+     * @param integer $id
+     * @return mixed
+     */
+    public function show($id)
+    {
+        return ['data' => Book::findOrFail($id)->toArray()];
+    }
+    /**
+     * POST /books
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function store(Request $request)
+    {
+        $book = Book::create($request->all());
+
+        return response()->json(['data' => $book->toArray()], 201, [
+            'Location' => route('books.show', ['id' => $book->id])
+        ]);
+    }
+        /**
+     * PUT /books/{id}
+     * @param Request $request
+     * @param $id
+     * @return mixed
+     */
+    public function update(Request $request, $id)
+    {
+        try {
+            $book = Book::findOrFail($id);
+        } catch (ModelNotFoundException $e) {
+            return response()->json([
+                'error' => [
+                    'message' => 'Book not found'
+                ]
+            ], 404);
+        }
+        $book->fill($request->all());
+        $book->save();
+        return ['data' => $book->toArray()];
+    }
+
+    /**
+     * DELETE /books/{id}
+     * @param $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function destroy($id)
+    {
+        try {
+            $book = Book::findOrFail($id);
+        } catch (ModelNotFoundException $e) {
+            return response()->json([
+                'error' => [
+                    'message' => 'Book not found'
+                ]
+            ], 404);
+        }
+        $book->delete();
+        return response(null, 204);
+    }
+}
--- tests/app/Http/Controllers/BooksControllerTest.php
+++ tests/app/Http/Controllers/BooksControllerTest.php
@@ -21,52 +21,47 @@ class BooksControllerTest extends TestCase
     {
         $books = ModelFactory::new()->count(2)->create();
         $this->get('/books');
-        foreach ($books as $book) {
-            $this->seeJson(['title' => $book->title]);
-        }
+        $expected = [
+            'data' => $books->toArray()
+        ];
+        $this->seeJsonEquals($expected);
     }
 
     /** @test */
     public function show_should_return_a_valid_book()
     {
         $book = ModelFactory::new()->create();
+        $expected = [
+            'data' => $book->toArray()
+        ];
         $this
-        ->get("/books/{$book->id}")
-        ->seeStatusCode(200)
-        ->seeJson([
-            'id' => $book->id,
-            'title' => $book->title,
-            'description' => $book->description,
-            'author' => $book->author
-        ]);
-        $data = json_decode($this->response->getContent(), true);
-        $this->assertArrayHasKey('created_at', $data);
-        $this->assertArrayHasKey('updated_at', $data);
+            ->get("/books/{$book->id}")
+            ->seeStatusCode(200)
+            ->seeJsonEquals($expected);
     }
     
-    /** @test  */
+    /** @test **/
     public function show_should_fail_when_the_book_id_does_not_exist()
     {
         $this
-            ->get('/books/99999')
+            ->get('/books/99999', ['Accept' => 'application/json'])
             ->seeStatusCode(404)
             ->seeJson([
-                'error' => [
-                    'message' => 'Book not found'
-                ]
+                'message' => 'Not Found',
+                'status' => 404
             ]);
     }
     
-    /** @test */
-    public function show_route_should_not_match_an_invalid_route()
-    {
-        $this->get('/books/this-is-invalid');
-        $this->assertDoesNotMatchRegularExpression(
-            '/Book not found/',
-            $this->response->getContent(),
-            'BooksController@show route matching when it should not.'
-        );
-    }
+   /** @test **/
+   public function show_route_should_not_match_an_invalid_route()
+   {
+       $this->get('/books/this-is-invalid');
+       $this->assertDoesNotMatchRegularExpression(
+           '/Book not found/',
+           $this->response->getContent(),
+           'BooksController@show route matching when it should not.'
+       );
+   }
     
     /** @test */
     public function store_should_save_new_book_in_the_database()
@@ -76,9 +71,17 @@ class BooksControllerTest extends TestCase
             'description' => 'An invisible man is trapped in the terror of his own creation',
             'author' => 'H. G. Wells'
         ]);
-        $this
-            ->seeJson(['created' => true])
-            ->seeInDatabase('books', ['title' => 'The Invisible Man']);
+        $body = json_decode($this->response->getContent(), true);
+        $this->assertArrayHasKey('data', $body);
+        $data = $body['data'];
+        $this->assertEquals('The Invisible Man', $data['title']);
+        $this->assertEquals(
+            'An invisible man is trapped in the terror of his own creation',
+            $data['description']
+        );
+        $this->assertEquals('H. G. Wells', $data['author']);
+        $this->assertTrue($data['id'] > 0, 'Expected a positive integer, but did not see one.');
+        $this->seeInDatabase('books', ['title' => 'The Invisible Man']);
     }
     
     /** @test */
@@ -102,6 +105,11 @@ class BooksControllerTest extends TestCase
             'description' => 'A science fiction masterpiece about Martians invading London',
             'author' => 'H.G Wells'
         ]);
+        $this->notSeeInDatabase('books', [
+            'title' => 'The War of the Worlds',
+            'description' => 'The book is way better than the movie.',
+            'author' => 'Wells, H. G.'
+        ]);
         $this->put("/books/{$book->id}", [
             'id' => 5,
             'title' => 'The War of the Worlds',
@@ -114,11 +122,14 @@ class BooksControllerTest extends TestCase
                 'id' => 1,
                 'title' => 'The War of the Worlds',
                 'description' => 'The book is way better than the movie.',
-                'author' => 'Wells, H. G.'
+                'author' => 'Wells, H. G.',
             ])
             ->seeInDatabase('books', [
                 'title' => 'The War of the Worlds'
             ]);
+        // Verify the data key in the response
+        $body = json_decode($this->response->getContent(), true);
+        $this->assertArrayHasKey('data', $body);            
     }
     
     /** @test */
